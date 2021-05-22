fe7cb4bf37b1f70f8cf4100a25282d75081f08b5
Hansangjin98 <onetable98@naver.com>
2021-05-23 03:24:12 +0900

    [Feat] 메인 뷰 데모

--- SOPKATHON_Kick/Sources/Home/HomeViewController.swift
+++ SOPKATHON_Kick/Sources/Home/HomeViewController.swift
@@ -9,22 +9,94 @@ import UIKit
 
 class HomeViewController: UIViewController {
 
+    @IBOutlet weak var topView: UIView!
+    @IBOutlet weak var scrollViewHeightConstraint: NSLayoutConstraint!
+    @IBOutlet weak var homeCollectionView: UICollectionView!
+    @IBOutlet weak var homeTableView: UITableView!
+    @IBOutlet weak var homePageControl: UIPageControl!
+    var homeCollectionViewList: [HomeCollectionViewModel] = []
+    var homeTableViewList: [HomeTableViewModel] = []
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        print("33")
-        // Do any additional setup after loading the view.
+        topView.backgroundColor = .main_Color
+        delegateSet()
+        homeTableViewSet()
+        homeCollectionViewSet()
+        setScrollHeight()
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    func homeTableViewSet() {
+        homeTableViewList.append(contentsOf: [
+            HomeTableViewModel(title: "테스트", contents: "테ㅔㅔㅔㅔㅔㅔㅔㅔㅔㅔㅔㅔㅔㅔㅔㅔ테스트", click: 32, minute: 15, like: 302),
+            HomeTableViewModel(title: "테스트", contents: "테ㅔㅔㅔㅔㅔㅔㅔㅔㅔㅔㅔㅔㅔㅔㅔㅔ테스트", click: 32, minute: 15, like: 302),
+            HomeTableViewModel(title: "테스트", contents: "테ㅔㅔㅔㅔㅔㅔㅔㅔㅔㅔㅔㅔㅔㅔㅔㅔ테스트", click: 32, minute: 15, like: 302),
+            HomeTableViewModel(title: "테스트", contents: "테ㅔㅔㅔㅔㅔㅔㅔㅔㅔㅔㅔㅔㅔㅔㅔㅔ테스트", click: 32, minute: 15, like: 302),
+            HomeTableViewModel(title: "테스트", contents: "테ㅔㅔㅔㅔㅔㅔㅔㅔㅔㅔㅔㅔㅔㅔㅔㅔ테스트", click: 32, minute: 15, like: 302),
+            HomeTableViewModel(title: "테스트", contents: "테ㅔㅔㅔㅔㅔㅔㅔㅔㅔㅔㅔㅔㅔㅔㅔㅔ테스트", click: 32, minute: 15, like: 302),
+            HomeTableViewModel(title: "테스트", contents: "테ㅔㅔㅔㅔㅔㅔㅔㅔㅔㅔㅔㅔㅔㅔㅔㅔ테스트", click: 32, minute: 15, like: 302),
+            HomeTableViewModel(title: "테스트", contents: "테ㅔㅔㅔㅔㅔㅔㅔㅔㅔㅔㅔㅔㅔㅔㅔㅔ테스트", click: 32, minute: 15, like: 302),
+            HomeTableViewModel(title: "테스트", contents: "테ㅔㅔㅔㅔㅔㅔㅔㅔㅔㅔㅔㅔㅔㅔㅔㅔ테스트", click: 32, minute: 15, like: 302),
+            HomeTableViewModel(title: "테스트", contents: "테ㅔㅔㅔㅔㅔㅔㅔㅔㅔㅔㅔㅔㅔㅔㅔㅔ테스트", click: 32, minute: 15, like: 302),
+            HomeTableViewModel(title: "테스트", contents: "테ㅔㅔㅔㅔㅔㅔㅔㅔㅔㅔㅔㅔㅔㅔㅔㅔ테스트", click: 32, minute: 15, like: 302),
+            HomeTableViewModel(title: "테스트", contents: "테ㅔㅔㅔㅔㅔㅔㅔㅔㅔㅔㅔㅔㅔㅔㅔㅔ테스트", click: 32, minute: 15, like: 302),
+            HomeTableViewModel(title: "테스트", contents: "테ㅔㅔㅔㅔㅔㅔㅔㅔㅔㅔㅔㅔㅔㅔㅔㅔ테스트", click: 32, minute: 15, like: 302)
+            
+        ])
     }
-    */
+    
+    func delegateSet() {
+        homeTableView.delegate = self
+        homeTableView.dataSource = self
+        homeCollectionView.delegate = self
+        homeCollectionView.dataSource = self
+    }
+    
+    func homeCollectionViewSet() {
+        homeCollectionViewList.append(contentsOf: [
+            HomeCollectionViewModel(imageName: "", title: "테ㅔㅔ스트", contents: "테ㅔㅔㅔㅔㅔㅔㅔㅔㅔㅔㅔㅔ테스트")
+        ])
+    }
+    
+    func setScrollHeight()
+    {
+        scrollViewHeightConstraint.constant = CGFloat(homeTableViewList.count * 59)
+        homeTableView.reloadData()
+    }
 
+}
+
+extension HomeViewController: UICollectionViewDelegate, UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return homeCollectionViewList.count
+    }
+
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath) as? HomeCollectionViewCell  else { return UICollectionViewCell() }
+        cell.image.image = homeCollectionViewList[indexPath.row].image
+        cell.title.text = homeCollectionViewList[indexPath.row].title
+        cell.content.text = homeCollectionViewList[indexPath.row].contents
+        
+        return cell
+
+    }
+}
+
+extension HomeViewController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        
+        return homeTableViewList.count
+    }
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 91
+    }
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath) as? HomeTableViewCell  else { return UITableViewCell() }
+        cell.title.text = homeTableViewList[indexPath.row].title
+        cell.content.text = homeTableViewList[indexPath.row].contents
+        cell.clickAndTime.text = "조회 " + "\(homeTableViewList[indexPath.row].click)" + "•" + "\(homeTableViewList[indexPath.row].minute)" + " 전"
+        cell.like.text = "\(homeTableViewList[indexPath.row].like)"
+        return cell
+    }
 }
